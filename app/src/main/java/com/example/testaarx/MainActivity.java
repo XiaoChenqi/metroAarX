f14a540a430959947c0148e487a7651d254de449
xiaocq <516563076@qq.com>
2021-06-08 10:14:48 +0800

    dier

--- app/src/main/java/com/example/testaarx/MainActivity.java
+++ app/src/main/java/com/example/testaarx/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.facilityone.wireless.demand.DemandActivity;
+import com.facilityone.wireless.demand.DemandCreateActivity;
+import com.facilityone.wireless.demand.fragment.DemandCreateFragment;
 import com.facilityone.wireless.maintenance.MaintenanceActivity;
 import com.facilityone.wireless.patrol.PatrolActivity;
 import com.facilityone.wireless.workorder.WorkOrderActivity;
@@ -19,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btn2;
     private Button btn3;
     private Button btn4;
+    private Button button5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +60,13 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivity(intent);
             }
         });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DemandCreateActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
     }
 
     private void initView() {
@@ -64,5 +74,6 @@ public class MainActivity extends AppCompatActivity {
         btn2 = (Button) findViewById(R.id.btn2);
         btn3 = (Button) findViewById(R.id.btn3);
         btn4 = (Button) findViewById(R.id.btn4);
+        button5 = (Button) findViewById(R.id.button5);
     }
 }
---
+++ demandcomponent/src/main/java/com/facilityone/wireless/demand/DemandCreateActivity.java
@@ -0,0 +1,69 @@
+package com.facilityone.wireless.demand;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.blankj.utilcode.util.ToastUtils;
+import com.facilityone.wireless.a.arch.base.FMFragment;
+import com.facilityone.wireless.a.arch.mvp.BaseFragmentActivity;
+import com.facilityone.wireless.componentservice.common.empty.EmptyFragment;
+import com.facilityone.wireless.componentservice.common.permissions.CommonConstant;
+import com.facilityone.wireless.demand.fragment.DemandCreateFragment;
+import com.facilityone.wireless.demand.fragment.DemandFragment;
+
+public class DemandCreateActivity extends BaseFragmentActivity
+        implements EmptyFragment.OnGoFragmentListener{
+    //再点一次退出程序时间设置
+    private static final long WAIT_TIME = 2000L;
+    private long TOUCH_TIME = 0L;
+    private EmptyFragment mInstance;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setSwipeBackEnable(false);
+    }
+
+    @Override
+    protected int getContextViewId() {
+        return R.id.demand_create_upload_id;//todo  xcq
+    }
+
+    @Override
+    protected FMFragment setRootFragment() {
+        mInstance = EmptyFragment.getInstance(CommonConstant.MESSAGE_DEMAND);
+        mInstance.setOnGoFragmentListener(this);
+        return mInstance;
+    }
+
+    @Override
+    public Object createPresenter() {
+        return null;
+    }
+
+    @Override
+    public void goFragment(Bundle bundle) {
+        mInstance.startWithPop(DemandCreateFragment.getInstance());
+    }
+
+    @Override
+    protected boolean isImmersionBarEnabled() {//fmfactivity中
+        return true;
+    }
+
+    @Override
+    public void onBackPressedSupport() {
+        if (getSupportFragmentManager().getBackStackEntryCount() > 1) {
+            pop();
+        } else {
+            if (System.currentTimeMillis() - TOUCH_TIME < WAIT_TIME) {
+                this.finish();
+            } else {
+                TOUCH_TIME = System.currentTimeMillis();
+                ToastUtils.showShort("再按一次退出");
+            }
+        }
+    }
+}
